845e667d360d36e90767515c917909ed47ed24d3
LanaAerts <Acer@DESKTOP-OGUJNRP.(none)>
2023-04-01 15:19:06 +0200

    configuration overview page

---
+++ src/pages/ConfiguratieOverzicht.vue
@@ -0,0 +1,36 @@
+<template>
+
+<H1 class='optionTitle'>Options</H1>
+<span class='configurationbuttons'>
+<button>Speltitel</button>
+<button>Spelassets</button>
+<button>Reclame</button>
+</span>
+
+
+</template>
+
+<style>
+
+.configurationbuttons button {
+background-color: plum;
+border-color: plum;
+color: #050505;
+margin: 1.5em;
+padding: 0.5em;
+padding-left: 1em ;
+padding-right: 1em;
+border-radius: 0.7em;
+}
+.configurationbuttons  {
+display: flex;
+justify-content: center;
+}
+.optionTitle {
+  display: flex;
+  justify-content: center;
+}
+
+
+
+</style>

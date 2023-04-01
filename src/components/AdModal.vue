fcdece5608a59e30194ab621382abe747a142adc
LanaAerts <Acer@DESKTOP-OGUJNRP.(none)>
2023-04-01 17:37:50 +0200

    Addings Modals for the configuration

---
+++ src/components/AdModal.vue
@@ -0,0 +1,22 @@
+<template>
+  <my-modal
+    :title="Ad"
+    :message="message"
+    @close="$emit('close')"
+  />
+
+  <h3>Upload hier jouw reclame</h3>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AdModal',
+  props: {
+    title: String,
+    message: String
+  }
+});
+</script>
---
+++ src/components/TitleModal.vue
@@ -0,0 +1,24 @@
+<template>
+  <my-modal
+    :title="title"
+    :message="message"
+    @close="$emit('close')"
+  />
+
+  <h3>Kies jouw titel</h3>
+
+  <q-input>Titel</q-input>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TitleModal',
+  props: {
+    title: String,
+    message: String
+  }
+});
+</script>

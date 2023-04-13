26fc0c8f375c48199fbb3f41683260a1a999ad69
LanaAerts <Acer@DESKTOP-OGUJNRP.(none)>
2023-04-13 21:57:57 +0200

    Finish Ad functionality

--- src/components/AdModal.vue
+++ src/components/AdModal.vue
@@ -3,10 +3,58 @@
     <h3 class="title">Upload hier jouw reclame</h3>
     <input class="Uploadbtn" type="file" @change="handleFileInput($event.target.files)">
     <div v-if="imageUrl">
-      <img class= "Image" :src="imageUrl" alt="Uploaded Image">
+      <img class="Image" :src="imageUrl" alt="Uploaded Image">
     </div>
+    <h6 class="Errormessage" v-if="errorMessage">{{ errorMessage }}</h6>
   </my-modal>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AdModal',
+  props: {
+    title: String,
+    message: String
+  },
+  data() {
+    return {
+      imageUrl: null as string | null,
+      errorMessage: '' as string
+    };
+  },
+  methods: {
+    handleFileInput(files: FileList) {
+      const file = files.item(0);
+      if (file) {
+        // Check file size
+        if (file.size > 1000000) {
+          this.errorMessage = 'Bestandsgrootte mag niet groter zijn dan 1MB.';
+          return;
+        }
+
+        // Check file type
+        const allowedTypes = ['image/png', 'image/svg+xml', 'image/jpeg'];
+        if (!allowedTypes.includes(file.type)) {
+          this.errorMessage = 'Alleen bestanden van het type PNG, SVG en JPEG/JPG zijn toegestaan.';
+          return;
+        }
+
+        // Set image URL and reset error message
+        this.imageUrl = URL.createObjectURL(file);
+        this.errorMessage = '';
+      }
+    }
+  },
+  beforeUnmount() {
+    // Clean up the URL created for the image to avoid memory leaks
+    if (this.imageUrl) {
+      URL.revokeObjectURL(this.imageUrl);
+    }
+  }
+});
+</script>
 
 <style lang="scss">
 .Image {
@@ -20,34 +68,9 @@
 .Uploadbtn{
   margin: 1em;
 }
+.Errormessage{
+  font-size: 1em;
+  margin: 1em;
+  color: darkred;
+}
 </style>
-
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  name: 'AdModal',
-  props: {
-    title: String,
-    message: String
-  },
-  data() {
-    return {
-      imageUrl: null as string | null
-    };
-  },
-  methods: {
-    handleFileInput(files: FileList) {
-      const file = files.item(0);
-      if (file) {
-        this.imageUrl = URL.createObjectURL(file);
-      }
-    }
-  },
-  beforeUnmount() {
-    if (this.imageUrl) {
-      URL.revokeObjectURL(this.imageUrl);
-    }
-  }
-});
-</script>

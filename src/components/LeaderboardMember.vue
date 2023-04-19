bfa5fe51e18eedb8951fb0389617875a944a23d0
Arne Didden <arne.didden@outlook.com>
2023-04-19 20:16:52 +0200

    hardcoded done enkel nog linken met service van Sem

---
+++ src/components/LeaderboardMember.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="lboard_mem">
+    <div class="name_bar">
+      <p><span>number here</span>name here</p>
+      <div class="bar_wrap">
+        <div class="inner_bar" style="width: 100%"></div>
+      </div>
+    </div>
+    <div class="points">points here</div>
+  </div>
+</template>
+
+<style lang="scss">
+.name_bar {
+  margin: 0 20px;
+  width: calc(100% - 150px);
+}
+
+.name_bar p {
+  color: #3a3d51;
+}
+
+.name_bar p span {
+  margin-left: 5px;
+}
+
+.bar_wrap {
+  height: 5px;
+  width: 100%;
+  background: #d5b000;
+  border-radius: 5px;
+  margin-top: 5px;
+  position: relative;
+  overflow: hidden;
+}
+
+.bar_wrap .inner_bar {
+  position: absolute;
+  top: 0%;
+  left: 0;
+  height: 5px;
+  background: #fff;
+  border-radius: 5px;
+  border-top-right-radius: 0px;
+  border-bottom-right-radius: 0px;
+}
+
+.points {
+  width: 100px;
+  color: #3a3d51;
+}
+
+.lboard_mem {
+  display: flex;
+  align-items: center;
+  padding: 15px 0;
+  border-bottom: 2px solid #ffe773;
+}
+
+.lboard_mem:first-child {
+  padding-top: 0;
+}
+</style>

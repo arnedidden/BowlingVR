22c9f807bc99d7e6bb5eff723a2f8981ef605d7e
Sem Radoes <semradijs@hotmail.com>
2023-06-05 01:31:27 +0200

    busy working on admin page

--- src/pages/AdminPage.vue
+++ src/pages/AdminPage.vue
@@ -3,17 +3,23 @@ import { defineComponent } from 'vue';
 import { useAuth } from 'src/services/auth.service';
 
 export default defineComponent({
-  setup() {
-    const { user } = useAuth();
-    return { user };
-  },
+    setup() {
+        const { user } = useAuth();
+        return { user };
+    },
 });
 </script>
-
 <template>
-  <h1>Admin</h1>
-  <p>
-    User: <strong>{{ user }}</strong>
-  </p>
-  <p>This page is only accessible to users that are logged in.</p>
+  <template v-if="user">
+    <h1>Admin</h1>
+    <p>
+      User: <strong>{{ user.github.username }}</strong>
+    </p>
+  </template>
+  <template v-else>
+    
+    <p>This page is only accessible to users that are logged in.</p>
+    
+  </template>
+
 </template>

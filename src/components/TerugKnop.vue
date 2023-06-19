e24c7915facd260e412766675df5011d926a0117
Gln <Glenn.1994@hotmail.com>
2023-06-19 21:51:05 +0200

    fixed: same backbutton on leaderbord + config page

--- src/components/TerugKnop.vue
+++ src/components/TerugKnop.vue
@@ -22,24 +22,31 @@ export default {
   align-items: center;
   justify-content: center;
   padding: 10px 20px;
-  border: 2px solid #fff;
+  border: 3px solid;
   margin-top: 10px;
   margin-left: 10px;
-  border-radius: 10px;
-  box-shadow: 4px 4px 8px black;
-  background-color: deeppink;
-  color: #fff;
+  letter-spacing: 2px;
+  font-family: 'Open Sans', sans-serif;
+  text-decoration: none;
+  text-transform: uppercase;
+  padding: 0.25em 0.5em;
+  box-shadow: 1px 1px 0px 0px, 2px 2px 0px 0px, 3px 3px 0px 0px, 4px 4px 0px 0px,
+    5px 5px 0px 0px;
+  background-color: #fff;
+  color: #000;
   font-size: 16px;
   cursor: pointer;
+  touch-action: manipulation;
 }
 
 .arrow {
   display: inline-block;
   width: 10px;
   height: 10px;
-  border-top: 2px solid #fff;
-  border-right: 2px solid #fff;
-  transform: rotate(-45deg);
+  border-top: 2px solid #000;
+  border-right: 2px solid #000;
+  transform: rotate(-135deg);
   margin-right: 6px;
+  color: #000;
 }
 </style>
